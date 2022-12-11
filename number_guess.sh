74829782f2dba32e4b9fe5dd728e39203b841bff
M1S7K <shumilinms@gmail.com>
2022-12-11 07:36:37 +0000

    feat: check entered username

--- number_guess.sh
+++ number_guess.sh
@@ -1,11 +1,19 @@
 #! /bin/bash
 #~~Number Guessing Game~~
-
+echo -e "\n~~Welcome to Number Guessing Game~~\n"
+#query DB
+PSQL="psql --username=freecodecamp --dbname=number_guessing_game -t --no-align -c"
 #generate random number
 echo $RANDOM
 
-#prompt for username
+#read username
 echo "Enter your username:"
 read USERNAME
 
-echo "Your user name is $USERNAME"
+#Usernames between 3 and 22 characters (Uppercase or lowercase letters with digits and the symbols: - and _ are allowed.)
+if [[ "$USERNAME" =~ ^[[:alpha:][:digit:]_-]{3,22}$ ]]
+then
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+else
+  echo "Sorry, this name is not allowed, try please another one"
+fi
